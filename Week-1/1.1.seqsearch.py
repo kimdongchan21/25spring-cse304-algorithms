6445683f3da7b101412f07b4fbc913f28bb215cd
kimdongchan21 <84613878+kimdongchan21@users.noreply.github.com>
2025-03-06 15:16:29 +0900

    Update 1.1.seqsearch.py

--- Week-1/1.1.seqsearch.py
+++ Week-1/1.1.seqsearch.py
@@ -1,7 +1,11 @@
 from typing import List
 
 def seqsearch(n: int, S: List[int], x: int) -> int: 
-    location = 0
+    location = 0;
+    while (location <= n-1 && S[location] != x)
+        location++;
+    if (location > n-1)
+    location = -1;
+    
 
-    # Complete the code here
     return location
